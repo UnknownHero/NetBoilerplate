2d7c541d1e9fb4631a4035edf840abaa6f2554e4
UnknownHero <lord365567921@gmail.com>
2013-05-12 16:55:15 +0400

    DAL Entity Framework init

---
+++ UnknownNetBoilerplate/DAL.EF/EntityFrameworkUnitOfWork.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using Infrastructure.DAL;
+
+namespace DAL.EF
+{
+    public class EntityFrameworkUnitOfWork : IUnitOfWork
+    {
+        public EntityFrameworkUnitOfWork(DbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+        public DbContext DbContext { get; protected set; }
+
+        public void Flush()
+        {
+            DbContext.SaveChanges();
+        }
+
+        public ITransaction BeginTransaction()
+        {
+            return new EntityFrameworkTransaction(this);
+        }
+
+        public void EndTransaction(ITransaction transaction)
+        {
+            if (transaction != null)
+            {
+                (transaction).Dispose();
+                transaction = null;
+            }
+        }
+
+        public void Insert<TEntity>(TEntity entity) where TEntity : class
+        {
+            DbContext.Set<TEntity>().Add(entity);
+        }
+
+        public void Update<TEntity>(TEntity entity) where TEntity : class
+        {
+            DbContext.Set<TEntity>().Attach(entity);
+        }
+
+        public void Delete<TEntity>(TEntity entity) where TEntity : class
+        {
+            DbContext.Set<TEntity>().Remove(entity);
+        }
+
+        public TEntity GetById<TEntity, TPrimaryKey>(TPrimaryKey id) where TEntity : class
+        {
+            return DbContext.Set<TEntity>().Find(id);
+        }
+
+        public IList<TEntity> GetAll<TEntity>() where TEntity : class
+        {
+            return DbContext.Set<TEntity>().ToList();
+        }
+
+        public void Dispose()
+        {
+            if (DbContext != null)
+            {
+                DbContext.SaveChanges();
+                (DbContext as IDisposable).Dispose();
+                DbContext = null;
+            }
+        }
+    }
+}
---
+++ UnknownNetBoilerplate/DAL.EF/EntityFrameworkUnitOfWorkFactory.cs
@@ -0,0 +1,54 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using Infrastructure.DAL;
+
+namespace DAL.EF
+{
+    /// <summary>
+    /// Unit of work factory implementation for Entity Framework.
+    /// Note: implementation based on extension Feature CTP4.
+    /// </summary>
+    public class EntityFrameworkUnitOfWorkFactory : IUnitOfWorkFactory
+    {
+        public EntityFrameworkUnitOfWorkFactory(string connectionString, DbModel dbModel)
+        {
+            this.ConnectionString = connectionString;
+			this.DbModel = dbModel;
+        }
+
+        protected string ConnectionString { get; private set; }
+
+		protected DbModel DbModel { get; private set; }
+
+        public IUnitOfWork BeginUnitOfWork()
+        {
+			return new EntityFrameworkUnitOfWork(
+				this.CreateDbContext()
+				);
+        }
+
+		private DbContext CreateDbContext()
+		{
+			return new DbContext(
+				this.ConnectionString
+				, this.DbModel.Compile()
+				);
+		}
+
+        public void EndUnitOfWork(IUnitOfWork unitOfWork)
+        {
+			var linqToSqlUnitOfWork = unitOfWork as EntityFrameworkUnitOfWork;
+			if (linqToSqlUnitOfWork != null)
+			{
+				linqToSqlUnitOfWork.Dispose();
+				linqToSqlUnitOfWork = null;
+			}
+        }
+
+        public void Dispose()
+        {
+			this.ConnectionString = null;
+			this.DbModel = null;
+        }
+	}
+}
