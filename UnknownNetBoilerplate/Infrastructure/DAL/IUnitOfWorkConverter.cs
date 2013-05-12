f97277c0de69ea1c33d969759eaa03c7ceca2c2d
UnknownHero <lord365567921@gmail.com>
2013-05-12 04:29:14 +0400

    DAL init

---
+++ UnknownNetBoilerplate/Infrastructure/DAL/IUnitOfWorkConverter.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace Infrastructure.DAL
+{
+    /// <summary>
+    /// Helper interface to convert <see cref="IUnitOfWork"/> to various data providers.
+    /// </summary>
+    public interface IUnitOfWorkConvertor
+    {
+        /// <summary>
+        /// Gets <see cref="IQueryable"/> from given <see cref="IUnitOfWork"/> implementation.
+        /// </summary>
+        IQueryable<TEntity> ToQueryable<TEntity>(IUnitOfWork unitOfWork) where TEntity : class;
+    }
+}
