01560dfcda8d24fa2b0c08047772e9bbf5654d98
UnknownHero <lord365567921@gmail.com>
2013-06-01 19:37:29 +0400

    Add Domain Workflow layer

--- UnknownNetBoilerplate/Domain.Workflows/Properties/AssemblyInfo.cs
+++ UnknownNetBoilerplate/Domain.Workflows/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Application")]
+[assembly: AssemblyTitle("Domain.Workflows")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Application")]
+[assembly: AssemblyProduct("Domain.Workflows")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("523a9284-5d9c-40bd-a796-3908ba4eff64")]
+[assembly: Guid("4278ecf4-0cdc-4f98-a3fd-4eeda74a04ee")]
 
 // Version information for an assembly consists of the following four values:
 //
